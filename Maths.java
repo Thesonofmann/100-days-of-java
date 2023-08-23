0d2f21ada57e0e18128e1b20808d41792a36d716
Jerry Ashh <jerryashh@gmail.com>
2023-08-23 06:26:17 +0100

    Day1/100

---
+++ Maths.java
@@ -0,0 +1,32 @@
+public class Maths{
+    public static void main(String[] args){
+        //Maths Operator
+        // BODMAS is applied in all mathematical/ arithmetic operation
+        System.out.println((int)Math.pow(11, 4));
+         // Arithmetic OPerators
+    //  int ten= 10;
+    //  int four = 4;
+    //  int six = 6;
+    //  int nine = 9;
+      System.out.println(10 + 2);   
+      System.out.println(10 - 2);
+      System.out.println(10 * 2);
+      System.out.println(10 % 2);
+      System.out.println(10 / 2);
+      System.out.println(10 + 2 -5 *6 / 2 * 4);
+      System.out.println((int)Math.sqrt(25));
+      System.out.println(Math.log(1101));
+
+
+      
+
+
+  // % is different from /
+  // / stands for division while % means remainder after division(modulus)
+
+
+
+    }
+    
+
+}
