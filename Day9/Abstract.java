a438326ab6fcc9df71083eabd0d47092a3f4ff84
Jerry Ashh <jerryashh@gmail.com>
2023-08-31 16:20:34 +0100

    Abstract method

--- Day9/Abstract.java
+++ Day9/Abstract.java
@@ -1,8 +1,17 @@
 package Day9;
+
 // Abstract class
 abstract class Abstract {
     public String fname = "SONxMAN";
     public int age = 22;
     public abstract void study(); //abstract method
 }
-//
+
+// Subclass (inherit from Abstract2)
+class Student extends Abstract {
+    public int graduationYear = 2020;
+    public void study() { // the body of the abstract method is provided here
+        System.out.println("Studying abstract but I have no idea what this is");
+    }
+
+}
---
+++ Day9/Abstract2.java
@@ -0,0 +1,15 @@
+package Day9;
+
+class Abstract2 {
+    public static void main(String[] args) {
+        //create an object of the Student class(which inherits attributes and methods from Abstract)
+        Student newObject = new Student();
+
+        newObject.study(); // calls the abstract method
+        System.out.println("Name: " + newObject.fname);
+        System.out.println("Age: " + newObject.age);
+        System.out.println("Graduation Year " + newObject.graduationYear);
+        
+
+    }
+}
