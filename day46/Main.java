f32cf809da68c4120454d130b6b135c3456370f2
Jerry Ashh <jerryashh@gmail.com>
2023-12-17 16:12:32 +0100

    Day46/100

---
+++ day46/Main.java
@@ -0,0 +1,12 @@
+package day46;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        //Object Factory
+        interface CalcSomething{
+            double calculate();
+        }
+    }
+}
